517068d548422d8eca86b30fb0c27f3d2fae2121
fedPass <federica.passanante@gmail.com>
2020-01-10 15:50:47 +0100

    inseriti nomi dei giorni

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 
     display_month(start_moment);
 
-    //intercetto il click sul button next_month
+    //------ intercetto il click sul button next_month
     $('#next_month').click(function(){
         //devo aggiungere 1 al mese corrente per passare al mese successivo
         start_moment.add(1, 'months');
@@ -20,7 +20,6 @@ $(document).ready(function(){
         if (current_month == 'Gennaio') {
             //fai comparire button prev
             $('#prev_month').show();
-
         }
         //se mi trovo su dicembre
         if (current_month == 'Dicembre') {
@@ -33,7 +32,7 @@ $(document).ready(function(){
         }
     });
 
-    //intercetto il click sul button prev_month
+    //------ intercetto il click sul button prev_month
     $('#prev_month').click(function(){
         //devo sottrarre 1 al mese corrente per passare al mese precedente
         start_moment.subtract(1, 'months');
@@ -58,29 +57,34 @@ $(document).ready(function(){
     function display_month(data_moment){
         //svuoto il calendario
         $('#calendario').empty();
+
         //clono l'oggetto moment per usarlo per il data-day
         var date = data_moment.clone();
+
         //numero giorni del mese da visualizzare
         var days_of_month = date.daysInMonth();
         //mese testuale
         var text_month = date.format('MMMM');
+        text_month = text_month.charAt(0).toUpperCase() + text_month.slice(1);
         //giorno della settimana in numero
         var day_of_week = date.day();
         //mese in numero
         var month = date.month();
         //anno in numero
         var year = date.year();
-        text_month = text_month.charAt(0).toUpperCase() + text_month.slice(1);
+
         //popolo dinaminamente il mese che appare come titolo
         $('#current_month').text(text_month);
+
         //devo stampare dei li vuoti per i giorni mancanti dall'inizio
         display_empty_block(day_of_week);
+
         //ciclo for per stampare i giorni del mese e data-day
         for (var i = 1; i <= days_of_month; i++) {
             // console.log(i + ' ' + text_month + ' ' + year);
             //uso template per appendere giorno corrente in html e data-day ad ogni giorno
             var context = {
-                'day': i + ' ' + text_month,
+                'day': date.format('ddd') + ' ' + i,
                 'date': date.format('YYYY-MM-DD')
             };
             var html_finale = template_function(context);
@@ -125,7 +129,7 @@ $(document).ready(function(){
                     //prendi il nome della festività
                     var holiday_name = holidays[i].name;
                     //controllo se la data è uguale a data-id, aggiungo classe vacation e append il nome della festività
-                    $('#calendario li[data-day="'+ holiday_date +'"]').addClass('vacation').append(' - ' + holiday_name);
+                    $('#calendario li[data-day="'+ holiday_date +'"]').addClass('vacation').append('<br>' + holiday_name);
                 }
             },
             'error': function(data){
